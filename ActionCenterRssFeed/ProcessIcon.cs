7b4ebd04d0887681f17db68daa49cce71311e867
Mark Tutkowski <tutkows1@gmail.com>
2017-12-21 00:46:53 -0600

    Get the toasts working.

---
+++ ActionCenterRssFeed/ProcessIcon.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+
+namespace ActionCenterRssFeed
+{
+    public class ActionCenterRssFeedApplicationContext : ApplicationContext
+    {
+        private NotifyIcon trayIcon;
+
+        public ActionCenterRssFeedApplicationContext()
+        {
+            // Initialize Tray Icon
+            trayIcon = new NotifyIcon()
+            {
+                Icon = new System.Drawing.Icon("D:\\Repos\\ActionCenterRssFeed\\ActionCenterRssFeed\\RSS.ico"),
+                ContextMenu = new ContextMenu(new MenuItem[] {
+                    new MenuItem("Add Notification", AddNotification),
+                    new MenuItem("Exit", Exit)
+                }),
+                Visible = true
+            };
+        }
+
+        private void AddNotification(object sender, EventArgs e)
+        {
+            XmlDocument toastXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastImageAndText04);
+
+            // Fill in the text elements
+            XmlNodeList stringElements = toastXml.GetElementsByTagName("text");
+            for (int i = 0; i < stringElements.Length; i++)
+            {
+                stringElements[i].AppendChild(toastXml.CreateTextNode("Line " + i));
+            }
+
+            // Specify the absolute path to an image
+            String imagePath = "file:///" + Path.GetFullPath("D:\\Repos\\ActionCenterRssFeed\\ActionCenterRssFeed\\RSS.png");
+            XmlNodeList imageElements = toastXml.GetElementsByTagName("image");
+            imageElements[0].Attributes.GetNamedItem("src").NodeValue = imagePath;
+
+            var blah = toastXml.GetXml();
+            ToastNotification toast = new ToastNotification(toastXml);
+            ToastNotificationManager.CreateToastNotifier("ActionCenterRssFeedAppUserModelID").Show(toast);
+        }
+
+        private void Exit(object sender, EventArgs e)
+        {
+            // Hide tray icon, otherwise it will remain shown until user mouses over it
+            trayIcon.Visible = false;
+
+            Application.Exit();
+        }
+    }
+}
--- ActionCenterRssFeed/Program.cs
+++ ActionCenterRssFeed/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows.Forms;
 
 namespace ActionCenterRssFeed
 {
@@ -10,6 +6,9 @@ namespace ActionCenterRssFeed
     {
         static void Main(string[] args)
         {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new ActionCenterRssFeedApplicationContext());
         }
     }
 }
