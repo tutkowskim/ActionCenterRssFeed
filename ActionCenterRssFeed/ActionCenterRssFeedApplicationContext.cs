ff5e984fb61b15b4536e660183a45c75f51aa9a3
Mark Tutkowski <tutkows1@gmail.com>
2017-12-22 18:39:37 -0600

    Each feed should have it's own timer and interval rate.

--- ActionCenterRssFeed/ActionCenterRssFeedApplicationContext.cs
+++ ActionCenterRssFeed/ActionCenterRssFeedApplicationContext.cs
@@ -1,58 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Timers;
 using System.Windows.Forms;
 
 namespace ActionCenterRssFeed
 {
     public class ActionCenterRssFeedApplicationContext : ApplicationContext
     {
-        
-        private List<RssFeed> rssFeeds;
-        private System.Timers.Timer timer;
         private NotifyIcon trayIcon;
+        private List<RssFeedReader> rssFeedReaders;
 
         public ActionCenterRssFeedApplicationContext()
         {
             // Hard code an RSS feed for now
-            rssFeeds = new List<RssFeed>();
-            rssFeeds.Add(new RssFeed("C:/Users/tutkowskim/Downloads/TestFeed.rss"));
-
-            // Create a timer to update the notifications
-            timer = new System.Timers.Timer()
-            {
-                AutoReset = true,
-                Enabled = true,
-                Interval = 500,
-            };
-            timer.Elapsed += new ElapsedEventHandler(UpdateNotifications);
+            rssFeedReaders = new List<RssFeedReader>();
+            rssFeedReaders.Add(new RssFeedReader("C:/Users/tutkowskim/Downloads/TestFeed.rss", 500));
 
             // Initialize Tray Icon
             trayIcon = new NotifyIcon()
             {
                 Icon = new System.Drawing.Icon(Assets.RssIconPath),
                 ContextMenu = new ContextMenu(new MenuItem[] {
-                    new MenuItem("Update Notifications", UpdateNotifications),
                     new MenuItem("Exit", Exit)
                 }),
                 Visible = true
             };
-        }
-
-        private void UpdateNotifications(object sender, EventArgs e)
-        {
-            foreach (var feed in rssFeeds)
-            {
-                IEnumerable<System.ServiceModel.Syndication.SyndicationItem> newRssFeedEntries = new List<System.ServiceModel.Syndication.SyndicationItem>(); ;
-                feed.UpdateRssFeed(out newRssFeedEntries);
-
-                string feedTitle = feed.Title;
-
-                foreach (var item in newRssFeedEntries)
-                {
-                    ToastManager.Toast(item.Title.Text, feedTitle, item.Summary.Text);
-                }
-            }
         }
 
         private void Exit(object sender, EventArgs e)
---
+++ ActionCenterRssFeed/RssFeedReader.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Timers;
+using System.Xml;
+
+namespace ActionCenterRssFeed
+{
+    public class RssFeedReader
+    {
+        private Timer _timer;
+        public string RssFeedUri { get; set; }
+        public double FeedUpdateInterval
+        {
+            get
+            {
+                return _timer != null ? _timer.Interval : -1;
+            }
+            set
+            {
+                if (_timer != null)
+                {
+                    _timer.Interval = value;
+                }
+            }
+        }
+        
+        public SyndicationFeed Feed
+        {
+            get;
+            private set;
+        }
+
+        public RssFeedReader(string uri, double feedUpdateInterval)
+        {
+            RssFeedUri = uri;
+
+            // Create a timer to update the notifications
+            _timer = new Timer()
+            {
+                AutoReset = true,
+                Enabled = true,
+                Interval = feedUpdateInterval,
+            };
+            _timer.Elapsed += new ElapsedEventHandler(UpdateFeedAndSendNotifications);
+        }
+
+        private void UpdateFeedAndSendNotifications(object sender, ElapsedEventArgs e)
+        {
+            IEnumerable<SyndicationItem> newRssFeedEntries;
+            UpdateRssFeed(out newRssFeedEntries);
+            foreach (var item in newRssFeedEntries)
+            {
+                ToastManager.Toast(item.Title.Text, Feed.Title.Text, item.Summary.Text);
+            }
+        }
+
+        private void UpdateRssFeed(out IEnumerable<SyndicationItem> newItems)
+        {
+            // Save off the current feed
+            SyndicationFeed oldFeed = Feed;
+
+            // Update the feed
+            using (var reader = XmlReader.Create(RssFeedUri))
+            {
+                Feed = SyndicationFeed.Load(reader);
+            }
+
+            // Determine the new items
+            if (Feed == null)
+            {
+                newItems = new List<SyndicationItem>();
+            }
+            else if (oldFeed == null)
+            {
+                newItems = Feed.Items;
+            }
+            else
+            {
+                newItems = Feed.Items.Where(item => !oldFeed.Items.Any(oldItem => oldItem.Id == item.Id));
+            }
+        }
+    }
+}
