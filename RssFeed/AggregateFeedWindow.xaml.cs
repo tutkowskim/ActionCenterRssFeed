85b52f1adcabfd317ea9dfd9259bb75a9fc6e3a1
Mark Tutkowski <tutkows1@gmail.com>
2017-12-30 18:33:25 -0600

    Fit the web browser to the content it is showing and hide the scroll bar.

--- RssFeed/AggregateFeedWindow.xaml.cs
+++ RssFeed/AggregateFeedWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Windows;
@@ -14,6 +15,7 @@ namespace RssFeed
         private readonly static log4net.ILog logger = log4net.LogManager.GetLogger("tutkowski.rssfeed.aggregatefeedwindow");
 
         private IEnumerable<FeedReader> _feedReaders;
+        private ObservableCollection<FeedItem> _feedItems = new ObservableCollection<FeedItem>();
 
         /// <summary>
         /// Constructor
@@ -31,8 +33,11 @@ namespace RssFeed
                 reader.PropertyChanged += ResfreshItems;
             }
 
+            // Set the source of the list view
+            FeedItems.ItemsSource = _feedItems;
+
             // Set the current items in the feeds
-            OrderItemsByPublishDate();
+            ResfreshItems(null, null);
         }
 
         /// <summary>
@@ -42,33 +47,47 @@ namespace RssFeed
         /// <param name="e"></param>
         private void ResfreshItems(object sender, EventArgs e)
         {
-            OrderItemsByPublishDate();
+            Dispatcher.BeginInvoke(new Action(delegate ()
+            {
+                OrderItemsByPublishDateAndDisplay();
+            }));
         }
 
         /// <summary>
         /// Get the items to show, sort them, and display them.
         /// </summary>
-        private void OrderItemsByPublishDate()
+        private void OrderItemsByPublishDateAndDisplay()
         {
-            List<FeedItem> updatedItemList = new List<FeedItem>();
-
-            // Add all of the items to a temporay list
+            List<FeedItem> updatedFeedItemList = new List<FeedItem>();
             foreach (FeedReader reader in _feedReaders)
             {
-                if(reader.Feed != null)
+                if (reader.Feed != null)
                 {
-                    updatedItemList.AddRange(reader.Feed.Items);
+                    updatedFeedItemList.AddRange(reader.Feed.Items);
                 }
             }
 
-            // Sort the items
-            updatedItemList.Sort((item1, item2) => item2.PublishDate.CompareTo(item1.PublishDate));
-
-            // Update the list
-            Dispatcher.BeginInvoke(new Action(delegate ()
+            // Remove any items that no longer exist
+            var removedItems = _feedItems.Where(item => !updatedFeedItemList.Any(updatedItem => item.Id == item.Id));
+            foreach (var item in removedItems)
             {
-                FeedItems.ItemsSource = updatedItemList;
-            }));
+                _feedItems.Remove(item);
+            }
+
+            // Add any new items
+            var newItems = updatedFeedItemList.Where(updatedItem => !_feedItems.Any(item => updatedItem.Id == item.Id));
+            foreach (var item in newItems)
+            {
+                _feedItems.Add(item);
+            }
+
+            // Resort the list if neccissary
+            var sortableList = new List<FeedItem>(_feedItems);
+            sortableList.Sort((item1, item2) => item2.PublishDate.CompareTo(item1.PublishDate));
+            for (int i = 0; i < sortableList.Count; ++i)
+            {
+                _feedItems.Move(_feedItems.IndexOf(sortableList[i]), i);
+            }
         }
 
         /// <summary>
@@ -90,7 +109,12 @@ namespace RssFeed
             }
         }
 
-        private void UpdateWebBrowserContent(object sender, EventArgs e)
+        /// <summary>
+        /// After the web browser for a feed item is initialized set it's content.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void WebBrowser_Initialized(object sender, EventArgs e)
         {
             WebBrowser browser = sender as WebBrowser;
             FeedItem item = browser.DataContext as FeedItem;
@@ -102,6 +126,21 @@ namespace RssFeed
             {
                 logger.Warn("Unable to determine the webbrowser loaded.");
             }
+        }
+
+        /// <summary>
+        /// After the content is loaded for a feed item set the Height so that all the content is visable.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void WebBrowser_LoadCompleted(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+            WebBrowser webBrowser = sender as WebBrowser;
+            mshtml.HTMLDocument doc = webBrowser.Document as mshtml.HTMLDocument;
+            mshtml.IHTMLElement2 elem = doc.activeElement as mshtml.IHTMLElement2;
+
+            doc.body.style.overflow = "hidden";
+            webBrowser.Height = elem.scrollHeight;
         }
     }
 }
